ec4f9e81a75262896dbd819d21cf9bac6baa1a2f
sebas200731 <158625964+sebas200731@users.noreply.github.com>
2024-05-27 10:34:46 -0500

    Add files via upload

---
+++ adivinanza.py
@@ -0,0 +1,8 @@
+print("Bienvenido al juego de adivina el numero")
+
+num=int(input("De acuerdo a tu intuicion, ingresa un numero de 1 a 5 y verifica si es correcto: "))
+if num==4:
+    print ("¡Felicidades, elegiste el número correcto!")
+else:
+    print ("Lo siento, te equivocaste")  
+        
